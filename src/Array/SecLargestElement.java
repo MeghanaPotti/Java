016092beb713b8f80fcfe329704e9a5be56b7148
pottimeghana123@gmail.com <pottimeghana123@gmail.com>
2025-04-21 14:27:04 +0530

    modfications

--- src/Array/SecLargestElement.java
+++ src/Array/SecLargestElement.java
@@ -4,8 +4,8 @@ public class SecLargestElement {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {12,35,436,3,745};
-		int largest = -1, secondLargest = -1;
+		int[] arr = {12,35,436,3,745,724};
+		int largest = 0, secondLargest = 0;
 		
 		for(int i=0;i<arr.length; i++) {
 			if(arr[i]>largest) {
