2eda96e88284c537ce2e7bb4d60dd478b688fc30
pottimeghana123@gmail.com <pottimeghana123@gmail.com>
2025-04-22 12:51:15 +0530

    modifications

--- src/Strings/IndexesOfChar.java
+++ src/Strings/IndexesOfChar.java
@@ -6,14 +6,16 @@ public class IndexesOfChar {
 
 		String a = "qertuywwqwrqqqqweqa";
 		char target = 'q';
+		int count = 0;
 		
 		System.out.print(target+" indexes are ");
 		for(int i=0; i<a.length(); i++) {
 			if(a.charAt(i)==target) {
 				System.out.print(i+" ");
+				count++;
 			}
 		}
-		
+		System.out.println(target+"-"+count);
 	}
 
 }
